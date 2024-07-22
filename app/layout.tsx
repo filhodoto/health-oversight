1366b56af95e287adf1ea9ace6f3db1766cae5a4
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-07-22 14:54:47 +0100

    Add base css and tailwind configurations for styles in our app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
-import { cn } from '/@/lib/utils';
+import { cn } from '@/lib/utils';
+
 const fontSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600'],
@@ -20,7 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={fontSans.className}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-dark-300 font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
