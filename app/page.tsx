161f82e961d4c90206fa25340601f243454e36f4
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-07-22 15:09:40 +0100

    Setup project to use dark theme with shadcn and add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Home = () => {
-  return <div className="text-3xl text-gray-800">Home</div>;
+  return (
+    <>
+      <div className="text-3xl text-gray-800">Home</div>
+      <Button>Shad CN Btn</Button>
+    </>
+  );
 };
 
 export default Home;
