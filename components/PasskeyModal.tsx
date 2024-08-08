5dea72e897e91228b242c2d3d8b59b49d3dd838e
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-08-08 11:54:59 +0100

    Create validation for passkey modal using crypto and local storage

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -15,17 +15,25 @@ import {
 } from '@/components/ui/input-otp';
 
 import { ICONS_URL } from '@/constants';
+import { decryptKey, encryptKey } from '@/lib/utils';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+// TODO:: Create token validation with server connection instead fo local storage encrypt
 const PasskeyModal = () => {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const [passkey, setPasskey] = useState('');
   const [error, setError] = useState('');
 
-  // TODO:: Not sure about this way of doing things
+  // Get encrypted key from local storage
+  // * Note:: We need to check for type fo window because of NextJS specifics
+  const accessData =
+    typeof window !== 'undefined'
+      ? localStorage && window.localStorage.getItem('encryptedData')
+      : null;
+
   const closeModal = () => {
     // Close modal
     setIsOpen(false);
@@ -38,10 +46,42 @@ const PasskeyModal = () => {
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     e.preventDefault();
-    //TODO:: Add correct validation
-    // Close modal
-    setError('Something went wrong');
+
+    // Check if entered passkey is equal to our passkey defined on .env (not sure if this is where passkey should be defined)
+    if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      // 1. Create encrypt key so we can store it in local storage and tell browser user is authenticated
+      const encryptedData = encryptKey(passkey);
+
+      // 2. Store the encrypt key object in local storage
+      // * NOTE:: Storing the encryption key and IV in local storage is highly insecure. For production we should use token authentication
+      localStorage.setItem('encryptedData', JSON.stringify(encryptedData));
+
+      // 3. Navigate to admin
+      router.push('/admin');
+    } else {
+      // Show error
+      setError('Something went wrong');
+    }
   };
+
+  useEffect(() => {
+    // Check if there's a encrypted key object in local storage. If so see if it's valid
+    if (accessData) {
+      // Decrypt passkey object and get passkey
+      const passkey = decryptKey(JSON.parse(accessData));
+
+      // Compare it to NEXT_PUBLIC_ADMIN_PASSKEY to know if we navigate user to admin
+      if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+        // Navigate to admin page
+        router.push('/admin');
+      } else {
+        setIsOpen(true);
+      }
+    } else {
+      setIsOpen(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accessData]);
 
   return (
     <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
